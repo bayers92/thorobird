10099bf57c6405acf1aac8c9eb861c6f602e7a59
bayers92 <brianc.ayers@gmail.com>
2015-04-03 10:00:01 -0400

    contact form

---
+++ contact-form-handler.php
@@ -0,0 +1,42 @@
+<?php 
+$errors = '';
+$myemail = 'brianc.ayers@gmail.com';
+if(empty($_POST['name'])  || 
+   empty($_POST['email'])) 
+{
+    $errors .= "\n Error: name and email fields are required";
+}
+
+$name = $_POST['name']; 
+$email = $_POST['email']; 
+$subject = $_POST['subject']; 
+$message = $_POST['message']; 
+
+if( empty($errors))
+{
+	$to = $myemail; 
+	$email_subject = "Contact form submission: $name";
+	$email_body = "You have received a new message. ".
+	" Here are the details:\n Name: $name \n Email: $email \n Subject: $subject \n Message: $message"; 
+
+	
+	mail($to,$email_subject,$email_body,$headers);
+	//redirect to the 'thank you' page
+	header('Location: contact.html');
+} 
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<html>
+<head>
+	<title>Contact form handler</title>
+</head>
+
+<body>
+<!-- This page is displayed only if there is some error -->
+<?php
+echo nl2br($errors);
+?>
+
+
+</body>
+</html>
