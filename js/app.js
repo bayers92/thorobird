da6e81ec466a8a4d1b235819694da98a3f727926
bayers92 <brianc.ayers@gmail.com>
2015-04-03 11:11:35 -0400

    mobile nav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).foundation({
+  offcanvas : {
+    close_on_click : true
+  }
+});
